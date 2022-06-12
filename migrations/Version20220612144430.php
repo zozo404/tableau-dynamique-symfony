490bdf944373da6ce3e6c296a1c8118a09cc1add
zozo404 <enzocuny66@gmail.com>
2022-06-12 16:49:29 +0200

    :art: | mise en place sur pc portable

--- migrations/Version20220612144430.php
+++ migrations/Version20220612144430.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220529105103 extends AbstractMigration
+final class Version20220612144430 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,16 @@ final class Version20220529105103 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE estate (id INT AUTO_INCREMENT NOT NULL, address VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE products (id INT AUTO_INCREMENT NOT NULL, address VARCHAR(59) DEFAULT NULL, city VARCHAR(21) DEFAULT NULL, name VARCHAR(24) DEFAULT NULL, price NUMERIC(7, 1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE products CHANGE address address VARCHAR(59) DEFAULT NULL, CHANGE city city VARCHAR(21) DEFAULT NULL, CHANGE name name VARCHAR(24) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE estate');
+        $this->addSql('DROP TABLE products');
         $this->addSql('DROP TABLE messenger_messages');
-        $this->addSql('ALTER TABLE products CHANGE address address VARCHAR(59) DEFAULT NULL COLLATE `utf8_general_ci`, CHANGE city city VARCHAR(21) DEFAULT NULL COLLATE `utf8_general_ci`, CHANGE name name VARCHAR(24) DEFAULT NULL COLLATE `utf8_general_ci`');
     }
 }
